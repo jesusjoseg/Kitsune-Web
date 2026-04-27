1ab3c8a9c5bd41f1d13a0ae30397ecc5e31ff592
jesusjoseg <jesusjosegardea@gmail.com>
2026-04-27 13:36:21 -0600

    regristo

--- Registro.php
+++ Registro.php
@@ -10,7 +10,24 @@
 <body>
     <?php include 'Php/header.php'; ?>
     <main class="regristo-main">
-        <div class=""></div>
+        <div class="container registro-container">
+            <div class="registro-info">
+                <h2>¡Esta a un paso de la <span class="orange">V0.8</span>!</h2>
+                <ul class="beneficios-list">
+                    <li><i class="fas fa-check orange"></i></li>
+                    <li><i class="fas fa-check orange"></i></li>
+                    <li><i class="fas fa-check orange"></i></li>
+                    <li><i class="fas fa-check orange"></i></li>
+                </ul>
+                <div class="precio-badge">
+                    <span>Precios de plan basico</span>
+                    <h3 class="orange">$800.00 MXN</h3>
+                </div>
+                <div class="precio-badge"></div>
+                <span>Precios de plan Pro</span>
+                <h3 class="orange">$1200.00 MXN</h3>
+            </div>
+        </div>
     </main>
     <?php include 'Php/Footer.php'; ?>
 </body>
