79b85be3038584da76ff2e6ffbd91e9d5cfe163e
jesusjoseg <jesusjosegardea@gmail.com>
2026-04-27 13:56:58 -0600

    formulario

--- Registro.php
+++ Registro.php
@@ -6,6 +6,7 @@
     <title>Registro -- Kitsune POS</title>
     <link rel="icon" type="image/x-icon" href="Src/Image/gemini-svg.ico">
     <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
     <?php include 'Php/header.php'; ?>
@@ -14,7 +15,15 @@
             <div class="registro-info">
                 <h2>¡Esta a un paso de la <span class="orange">V0.8</span>!</h2>
                 <ul class="beneficios-list">
+                    <h3>Plan basico</h3>
                     <li><i class="fas fa-check orange"></i></li>
+                    <li><i class="fas fa-check orange"></i></li>
+                    <li><i class="fas fa-check orange"></i></li>
+                    <li><i class="fas fa-check orange"></i></li>
+                </ul>
+                <ul class="beneficios-list">
+                    <h3>Plan PRO</h3>
+                    <li><i class="fas fa-check orange"></i> Los mismo de plan basico</li>
                     <li><i class="fas fa-check orange"></i></li>
                     <li><i class="fas fa-check orange"></i></li>
                     <li><i class="fas fa-check orange"></i></li>
@@ -27,6 +36,15 @@
                 <span>Precios de plan Pro</span>
                 <h3 class="orange">$1200.00 MXN</h3>
             </div>
+        </div>
+        <div class="registro-card">
+            <h3>Crea tu Cuenta</h3>
+            <p></p>
+            <form action="">
+                <label for="Nombre">Nombre:</label>
+                <input type="text" name="" id="">
+                <input type="submit" value="Regtista">
+            </form>
         </div>
     </main>
     <?php include 'Php/Footer.php'; ?>
