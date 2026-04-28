3e6711fc1b143db045a2c3749af33263ce5972e3
jesusjoseg <jesusjosegardea@gmail.com>
2026-04-27 18:22:34 -0600

    formulario

--- Login.php
+++ Login.php
@@ -3,25 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Iniciar Sesión -- Kitsune POS</title>
     <link rel="stylesheet" href="Style.css">
     <link rel="icon" type="image/x-icon" href="Src/gemini-svg.ico">
 </head>
 </head>
 <body>
     <?php include 'Php/header.php'; ?>
-    <h1>Iniciar Sesión</h1>
-    <div>
-    <form action="Php/ProcessLogin.php" method="post">
-        <label for="Correo">Correo</label>
-        <input type="email" id="Correo" name="Correo" required>
-        <label for="Contraseña">Contraseña</label>
-        <input type="password" id="Contraseña" name="Contraseña" required>
-        <p>¿Olvidates tu contraseña? <a href="Php/ResetPassword.php">Restablecer</a></p>
-        <input type="submit" value="Iniciar Sesión">
-    </form>
-    <a href="Registro.php" class="Regristo">¿No tienes cuenta? Regístrate aquí</a>
-    </div>
+    <main class="login-container">
+        
+        <h1>Iniciar Sesión</h1>
+        <div>
+            <form action="Php/ProcessLogin.php" method="post">
+                <div class="input-group">
+                    <label for="Correo">Correo</label>
+                    <input type="email" id="Correo" name="Correo" required>
+                </div>
+                <div class="input-group">
+                    <label for="Contraseña">Contraseña</label>
+                    <input type="password" id="Contraseña" name="Contraseña" required>
+                </div>
+                
+                <p>¿Olvidates tu contraseña? <a href="Php/ResetPassword.php">Restablecer</a></p>
+                <input type="submit" value="Iniciar Sesión">
+            </form>
+            <div>
+                <a href="Registro.php" class="Regristo">¿No tienes cuenta? Regístrate aquí</a>
+            </div>
+        </div>
+    </main>
     <?php include 'Php/Footer.php' ?>
 </body>
 </html>
--- Registro.php
+++ Registro.php
@@ -15,21 +15,21 @@
             <div class="registro-info">
                 <h2>¡Esta a un paso de la <span class="orange">V0.8</span>!</h2>
                 <ul class="beneficios-list">
-                    <h3>Plan basico</h3>
-                    <li><i class="fas fa-check orange"></i></li>
-                    <li><i class="fas fa-check orange"></i></li>
-                    <li><i class="fas fa-check orange"></i></li>
-                    <li><i class="fas fa-check orange"></i></li>
+                    <h3>Plan Básico</h3>
+                    <li><i class="fas fa-check orange"></i> Punto de Venta y Control de Inventarios</li>
+                    <li><i class="fas fa-check orange"></i> Configuración de Ticket Personalizado</li>
+                    <li><i class="fas fa-check orange"></i> Licencia Vitalicia Kitsune POS</li>
+                    <li><i class="fas fa-check orange"></i> Soporte para 1 Sucursal Física</li>
                 </ul>
                 <ul class="beneficios-list">
                     <h3>Plan PRO</h3>
-                    <li><i class="fas fa-check orange"></i> Los mismo de plan basico</li>
-                    <li><i class="fas fa-check orange"></i></li>
-                    <li><i class="fas fa-check orange"></i></li>
-                    <li><i class="fas fa-check orange"></i></li>
+                    <li><i class="fas fa-check orange"></i> Los mismo de plan Básico</li>
+                    <li><i class="fas fa-check orange"></i> Creacion de reporte Diario</li>
+                    <li><i class="fas fa-check orange"></i> Gestion de Administración de crédito</li>
+                    <li><i class="fas fa-check orange"></i> Registro de Clientes y Saldos Pendientes</li>
                 </ul>
                 <div class="precio-badge">
-                    <span>Precios de plan basico</span>
+                    <span>Precios de plan Básico</span>
                     <h3 class="orange">$800.00 MXN</h3>
                 </div>
                 <div class="precio-badge"></div>
@@ -40,10 +40,33 @@
         <div class="registro-card">
             <h3>Crea tu Cuenta</h3>
             <p></p>
-            <form action="">
-                <label for="Nombre">Nombre:</label>
-                <input type="text" name="" id="">
-                <input type="submit" value="Regtista">
+            <form action="Php/ProcessRegistro.php" method="POST" class="form-registro">
+                <div class="input-group">
+                    <label for="Nombre">Nombre:</label>
+                    <input type="text" name="Nombre" id="Nombre" required>
+                </div>
+                <div class="input-group">
+                    <label for="Apellido">Apellido:</label>
+                    <input type="text" name="Apellido" id="Apellido" required>
+                </div>
+                <div class="input-group">
+                    <label for="Correo">Correo:</label>
+                    <input type="email" name="Correo" id="Correo" placeholder="tunombre@ejemplo.com"required>
+                </div>
+                <div class="input-group">
+                    <label for="Password">Contraseña:</label>
+                    <input type="password" name="Contrasena1" id="Contrasena1" placeholder="Contraseña" required>
+                </div>
+                <div class="input-group">
+                    <label for="Password">Reperti Contraseña:</label>
+                    <input type="password" name="ConfirmarContrasena" id="ConfirmarContrasena" placeholder="Contraseña" required>
+                </div>
+                <div class="input-group">
+                    <label for="Telefono">Telefono:</label>
+                    <input type="tel" id="Telefono" name="Telefono" placeholder="123-456-7890">
+                </div>
+
+                <input type="submit" value="Registrar">
             </form>
         </div>
     </main>
