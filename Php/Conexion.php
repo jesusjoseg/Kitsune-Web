c332462def84c6c9870c9c0fdd1a70cf1018fba0
jesusjoseg <jesusjosegardea@gmail.com>
2026-04-28 11:48:22 -0600

    conexion de datos

--- Php/Conexion.php
+++ Php/Conexion.php
@@ -1,26 +1,23 @@
 <?php
-function loadenv($path) {
-    if (!file_exists($path)) return;
-    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    $Env = [];
-    foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) continue; // Ignorar comentarios
-        list($key, $value) = explode('=', $line, 2);
-        $Env[trim($key)] = trim($value);
-        }
-        return $Env;
-    }   
-loadenv(__DIR__.'/.env');
-    $host=$Env['DB_HOST'];
-    $user=$Env['DB_USER'];
-    $password=$Env['DB_PASSWORD'];
-    $database=$Env['DB_NAME'];
-    $Conexion = new mysqli($host, $user, $password, $database);
-    if ($Conexion->connect_error) {
-        die("Error de conexión: " . $Conexion->connect_error);
-   }
-   $Conexion->set_charset("utf8");
-
-
-
+function loadenv($path){
+    if(!file_exists($path)) return[];
+    $lines =file($path,FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+    $Env=[];
+    foreach ($lines as $line){
+        if (strpos(trim($line),'#')===0)continue;
+        list($key, $value)=explode('=',$line,2);
+        $Env[trim($key)]=trim($value);
+    }
+    return $Env;
+}
+$Env =loadenv(__DIR__.'/.env');
+$host =$Env['DB_HOST']??'';
+$user= $Env['DB_USER']??'';
+$password =$Env['DB_PASSWORD']??'';
+$datebase= $Env['DB_NAME']??'';
+$Conexion = mysqli_connect($host,$user,$password,$datebase);
+if ($Conexion->connect_error){
+    die("Erro de conexion:".$Conexion->connect_error);
+}
+$Conexion->set_charset("utf8")
 ?>
