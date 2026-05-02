3789293bfd944ef973f181f083641fd4d21becb9
jesusjoseg <jesusjosegardea@gmail.com>
2026-05-02 12:20:37 -0600

    stilo

--- Precio.php
+++ Precio.php
@@ -10,7 +10,46 @@
 </head>
 <body>
     <?php include 'Php/header.php'; ?>
-    
+    <main class="container">
+        <h1>Precio y Lincencias</h1>
+        <div class="price-grid">
+            <div class="price-card">
+                <h2>Lincecia Basico</h2>
+                <p class="price">$800.00<span class="orange">MXN</span></p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <a href=""></a>
+            </div>
+            <div class="price-card">
+                <h2>Lincecia PRO</h2>
+                <p class="price">$1200.00 <span class="orange">MXN</span></p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="price-card">
+                <h2>Pruebla Gratuita</h2>
+                <p class="price">$0.00 <span class="orange">MXN</span></p>
+                <ul>
+                    <li>Prueba gratuita de 30 dias</li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <a href="Descarga.php">Probar gratis</a>
+                </ul>
+            </div>
+            
+        </div>
+    </main>
     <?php include 'Php/Footer.php'; ?>
 </body>
 </html>
