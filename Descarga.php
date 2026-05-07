e8d91a4890f8a7dd6042e30629edb8f9bc53dbc8
jesusjoseg <jesusjosegardea@gmail.com>
2026-05-06 22:55:37 -0600

    descarga

--- Descarga.php
+++ Descarga.php
@@ -13,6 +13,27 @@
     <main>
         <h1>Descarga</h1>
         <p>Bienvenido a la página de descarga.</p>
+        <div class="container">
+            <h2 >Obtener tu prueba gratuita</h2>
+            <p>Descargar el paquete comprimido con todo los necesarios para tener Kitsune</p>
+            <div class="Intalar">
+                <img src="" alt="">
+                <h3>Kitsune POS V0.8</h3>
+                <p></p>
+                <br>
+                <a href="Src/Descargar/KitsunePOS_Demo_V08.zip" class="btn-buy">Descargar (.ZIP)</a>
+            </div>
+            <div class="Intrucion">
+                <h4>Intruciones rapidas:</h4>
+                <ol>
+                    <li>Descargar el archivor <strong>.Zip</strong>.</li>
+                    <li>Haz click derecho y selecciona "Extraer todo".</li>
+                    <li>Ejecuta el archivo <strong>#####.exe</strong>.</li>
+                    <li>Si la aplicacion no abre espera uno minuto por que se esta creando todo la base de dato o si no volve a Ejecuta</li>
+                    <li>Tiene 30 dias de prueba gratis.</li>
+                </ol>
+            </div>
+        </div>
     </main>
     <?php include 'Php/Footer.php' ?>
 </body>
